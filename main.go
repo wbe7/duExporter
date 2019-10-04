b3ab63269112b7b97071add466eaf17cce4493f7
Mikhail Tikhonov <tikhonov_ma@mosoblbank.ru>
2019-10-04 15:45:51 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+var pathDir string
+
+func main() {
+	flag.StringVar(&pathDir, "p", ".", "Enter path to directory")
+	flag.Parse()
+	http.HandleFunc("/metrics", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("0.0.0.0:9100", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, checkDir())
+}
+
+func checkDir() string {
+	for {
+		size, _ := DirSize(pathDir)
+		fmt.Print("check_leveldb_store ")
+		fmt.Println(size)
+		o := "check_leveldb_store " + strconv.FormatInt(size, 10)
+		time.Sleep(5 * time.Second)
+		return o
+	}
+}
+
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
